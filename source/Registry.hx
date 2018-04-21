2dd6b454c0ecb49d78683d8e394b1d8dbebf8642
clue <colin.h.lu@gmail.com>
2018-04-21 11:09:47 -0500

    cleanup

--- source/Registry.hx
+++ source/Registry.hx
@@ -2,26 +2,7 @@ package;
 
 class Registry
 {
-    public static var levelList:Array<String> = [AssetPaths.trivial_level__tmx,
-                                                 AssetPaths.scale_intro__tmx,
-                                                 AssetPaths.best_level__tmx,
-                                                 AssetPaths.coin_intro__tmx,
-                                                 AssetPaths.coin_intro2__tmx,
-                                                 AssetPaths.two_coins__tmx,
-                                                 AssetPaths.three_balls__tmx,
-                                                 AssetPaths.gates_intro__tmx,
-                                                 AssetPaths.unscalable_trivial_intro__tmx,
-                                                 AssetPaths.unscalable_intro__tmx,
-                                                 AssetPaths.unscalable_ball__tmx,
-                                                 AssetPaths.wall_ball__tmx,
-                                                 AssetPaths.target_practice__tmx,
-                                                 AssetPaths.scale_ferry2__tmx,
-                                                 AssetPaths.gates_tricky__tmx,                                                 
-                                                 AssetPaths.target_practice2__tmx,
-                                                 AssetPaths.scale_dodge__tmx,
-                                                 AssetPaths.coin_square__tmx,
-                                                 AssetPaths.scale_ferry__tmx,
-                                                 AssetPaths.the_end__tmx];
+    public static var levelList:Array<String> = [AssetPaths.trivial_level__tmx,];
     public static var currLevel:Int = 0; 
     
     public static var tweenSem:Int = 0;
