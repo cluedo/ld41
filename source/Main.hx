94978a4da787c6c82a725e4754e61ad43db07470
clue <colin.h.lu@gmail.com>
2018-04-21 10:41:35 -0500

    placeholder game name

--- source/Main.hx
+++ source/Main.hx
@@ -9,6 +9,6 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(800, 600, PlayState));
+		addChild(new FlxGame(800, 600, PlayState, 1, 60, 60, true));
 	}
 }
