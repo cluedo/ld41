e6c927a6d1db746d5daf27d0ddec7d5b008d78ab
clue <colin.h.lu@gmail.com>
2018-04-21 19:31:13 -0500

    minor fix

--- source/Main.hx
+++ source/Main.hx
@@ -11,8 +11,8 @@ class Main extends Sprite
 		super();
 		addChild(new FlxGame(800, 600, PlayState, 1, 60, 60, true));
 		
-		var item1:MenuItem = new MenuItem("Hello");
-		var item2:MenuItem = new MenuItem("Hello2");
-		var menu:Menu = new Menu(0, [item1, item2]);
+		var item1 = new Menu.MenuItem("Hello");
+		var item2 = new Menu.MenuItem("Hello2");
+		var menu:Menu = new Menu.Menu(0, [item1, item2]);
 	}
 }
